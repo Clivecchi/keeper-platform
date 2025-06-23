ea2706ce5e25cb1f986e128891d79f5d398459ee
Chuck <clivecchi@gmail.com>
2025-06-22 21:01:02 -0400

    Monorepo restructure - Login Dev

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -10,4 +10,12 @@ export default defineConfig({
     },
   },
   base: '/',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
+  },
 }); 
